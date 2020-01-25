97e86ee1ee2c576e09afbfb62b587574802c33a7
Antonio Jurlina <antoniojurlina@Antonios-MacBook-Air.local>
2020-01-25 14:53:08 -0500

    adding scripts

--- Importing data.R
+++ Importing data.R
@@ -1,28 +1,61 @@
+#-------- packages --------
 library(dplyr)
 library(readr)
 library(stringr)
 library(tidyr)
+library(maps)
 
+#-------- importing data --------
 gdp_csv <- read_csv(paste0(paste0(here::here(), "/CAGDP2/"),
                            "CAGDP2__ALL_AREAS_2001_2018.csv"))
 
+county_data <- map_data("county")
+
+#-------- cleaning the data --------
 region_totals <- which(str_detect(gdp_csv$IndustryClassification, pattern = "All industry total"))
 
 gdp_regional <- gdp_csv[region_totals, ] %>%
-  separate(col = "GeoFIPS", into = c("FIPS", "location"), sep = '" ,"') %>%
-  separate(col = "location", into = c("County", "State"), sep = ", ") %>%
-  select(FIPS, County, State, Description, 11:27)
+  separate(col = "GeoFIPS", into = c("fips", "location"), sep = '" ,"') %>%
+  separate(col = "location", into = c("subregion", "region"), sep = ", ") %>%
+  select(fips, subregion, region, description = Description, 11:27) %>%
+  filter
 
-gdp_regional$State <- str_remove_all(gdp_regional$State, pattern = "/*")
+gdp_regional$region <- str_remove_all(gdp_regional$region, pattern = "\\*")
 
-gdp_regional <- gdp_regional[-which(is.na(gdp_regional$State)), ]
+gdp_regional$subregion[1838] <- "dona ana" 
+gdp_regional$subregion <- tolower(gdp_regional$subregion)
+
+gdp_regional <- gdp_regional[-which(is.na(gdp_regional$region)), ]
 
 gdp_regional[, 5:21] <- sapply(gdp_regional[, 5:21], as.numeric)
 
-columns_to_gather <- colnames(gdp_regional)[5:21]
+region_remove <- c(which(str_detect(gdp_regional$region, pattern = c("AK"))),
+                   which(str_detect(gdp_regional$region, pattern = c("HI"))),
+                   which(str_detect(gdp_regional$region, pattern = c("Staunton \\+ Waynesboro"))),
+                   which(str_detect(gdp_regional$region, pattern = c("Colonial Heights \\+ Petersburg"))),
+                   which(str_detect(gdp_regional$region, pattern = c("Fairfax City \\+ Falls Church"))),
+                   which(str_detect(gdp_regional$region, pattern = c("Manassas \\+ Manassas Park"))),
+                   which(str_detect(gdp_regional$region, pattern = c("Buena Vista \\+ Lexington"))))
 
-gdp_regional_long <- gather(gdp_regional, columns_to_gather, key = "Year", value = "GDP") %>%
-  mutate(Year = as.numeric(Year),
-         FIPS = as.integer(FIPS))
+gdp_regional <- gdp_regional[-region_remove, ]
+
+#-------- merging the gdp data with county lat and long data --------
+gdp_regional_region_names <- unique(gdp_regional$region)
+county_data_region_names <- unique(county_data$region)
+
+for(i in seq_along(gdp_regional_region_names)) {
+  gdp_regional$region[which(gdp_regional$region == gdp_regional_region_names[i])] <- county_data_region_names[i]
+}
+
+gdp_regional <- left_join(county_data, gdp_regional, by = c("subregion", "region"))
+
+#-------- turning the data frame from wide to long and saving it --------
+columns_to_gather <- colnames(gdp_regional)[9:25]
+
+gdp_regional_long <- gather(gdp_regional, columns_to_gather, key = "year", value = "gdp") %>%
+  mutate(year = as.numeric(year),
+         fips = as.integer(fips))
 
 save(gdp_regional_long, file = "gdp_regional_long.RData")
+
+
--- mapping script.R
+++ mapping script.R
@@ -1,15 +1,68 @@
+#-------- packages --------
 library(dplyr)
 library(ggplot2)
 library(ggthemes)
 library(gganimate)
 library(maps)
 
+#-------- importing data --------
 load("gdp_regional_long.RData")
 
-map("county")
+state_geo_data <- map_data("state")
 
-map_data("county")
-map_data("state")
+#-------- creating a map for a desired year --------
+design_color <- "snow"
+
+plot <- ggplot(data = gdp_regional_long %>%
+                 filter(year == 2017)) +
+  geom_polygon(aes(x = long, y = lat, group = group, fill = gdp), 
+               colour = design_color,
+               size = 0.02)
+
+plot <- plot + theme_map() +
+  coord_fixed(1.3)
+
+plot <- plot + geom_polygon(data = state_geo_data, 
+                            aes(x = long, y = lat, group = group), 
+                            colour = design_color, 
+                            fill = "transparent",
+                            size = 0.09)
+
+plot <- plot + viridis::scale_fill_viridis(option = "inferno", labels = scales::comma)
+
+plot <- plot + theme(panel.background = element_rect(fill = design_color, colour = design_color),
+             plot.background = element_rect(fill = design_color),
+             legend.background = element_rect(fill = "transparent"),
+             legend.title = element_text(face = "bold"))
+
+plot <- plot + labs(title = "U.S. GDP by County",
+                    subtitle = "2017",
+                    caption = "source: Bureau of Economic Analysis, U.S. Department of Commerce",
+                    fill = "GDP (thousands of 2019 USD)")
+
+plot <- plot +  
+  annotate(geom = "text", 
+           x = -73.5, 
+           y = 30, 
+           label = paste0("Total GDP: ", gdp_regional_long %>%
+                           filter(year == 2017) %>%
+                           select(gdp) %>%
+                           unique() %>%
+                           summarize(gdp = sum(gdp, na.rm = TRUE)) %>%
+                           pull(1) %>%
+                           scales::comma()),
+           size = 4) 
+
+ggsave("plot.png", plot, height = 7, width = 10)
+
+#-------- creating a gif (remove year filter if doing so) --------
+gif <- plot +
+  transition_time(year)
+
+anim <- animate(gif, height = 800, width = 1000)
+
+anim_save("gif.gif", anim)
+
 
 
 
